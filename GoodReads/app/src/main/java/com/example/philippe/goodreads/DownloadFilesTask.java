ad6f4e49e370121ea701a44663b9ba7636d808de
Philippe <philippe.marcotte@polymtl.ca>
2016-01-31 00:36:03 -0500

    bouton download fonctionnel

--- GoodReads/app/src/main/java/com/example/philippe/goodreads/DownloadFilesTask.java
+++ GoodReads/app/src/main/java/com/example/philippe/goodreads/DownloadFilesTask.java
@@ -1,36 +1,18 @@
 package com.example.philippe.goodreads;
 
-import android.media.session.MediaSession;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.google.api.client.auth.oauth2.AuthorizationCodeTokenRequest;
-import com.google.api.client.auth.oauth2.TokenResponse;
-import com.google.api.client.auth.oauth2.TokenResponseException;
-import com.google.api.client.http.BasicAuthentication;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.example.philippe.goodreadsapi.GoodreadsService;
+import com.example.philippe.goodreadsapi.Reviews;
 
 
-import org.scribe.model.Token;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.net.URL;
-import java.util.List;
-
-import oauth.signpost.OAuth;
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.OAuthProvider;
-import oauth.signpost.basic.DefaultOAuthConsumer;
-import oauth.signpost.basic.DefaultOAuthProvider;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-import oauth.signpost.exception.OAuthNotAuthorizedException;
-import oauth.signpost.signature.SignatureMethod;
 
 
 /**
@@ -42,35 +24,26 @@ class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
 
     final String ACCESS_TOKEN = "Y2yWPj3IZivAbQ7CAcXi0A";
     final String TOKEN_SECRET = "s5QOq80VQ8oIPtZdPcfephehEuGAGPGvHr7lwBiCTE";
-    protected Long doInBackground(URL... urls) {
-        // create a consumer object and configure it with the access
-        // token and token secret obtained from the service provider
-        //OAuthConsumer consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY,
-        //        CONSUMER_SECRET);
-        //consumer.setTokenWithSecret(ACCESS_TOKEN, TOKEN_SECRET);
+    private Context context;
 
-        // create an HTTP request to a protected resource
+    public DownloadFilesTask(Context context){
+        this.context = context;
+    }
+
+    protected Long doInBackground(URL... urls) {
         GoodreadsService.init(CONSUMER_KEY, CONSUMER_SECRET);
         GoodreadsService.setAccessToken(ACCESS_TOKEN, TOKEN_SECRET);
+        FileOutputStream outputStream;
+
         try {
+            outputStream = context.openFileOutput("toReadShelf",Context.MODE_PRIVATE);
             Reviews mimouShelves = GoodreadsService.getAllBooksOnShelf("to-read", "30067343");
-            boolean isInToRead = false;
-            int i = 0;
-            for (Review review: mimouShelves.getReviews()) {
-                isInToRead = false;
-                for(String shelfName:review.getShelves()){
-                    if(shelfName.equals("to-read"))
-                        isInToRead = true;
-                }
-                if(!isInToRead)
-                    Log.d("test","prout");
-                Log.d("book",Integer.toString(++i));
-            }
-            System.out.println();
+            Log.d("json", mimouShelves.jsonify().toString());
+            outputStream.write(mimouShelves.jsonify().toString().getBytes());
+            outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        OAuthConsumer consumer = new DefaultOAuthConsumer("iIlNngv1KdV6XzNYkoLA","exQ94pBpLXFcyttvLoxU2nrktThrlsj580zjYzmoM", SignatureMethod.PLAINTEXT);
         return 0l;
     }
 
--- GoodReads/app/src/main/java/com/example/philippe/goodreads/MainActivity.java
+++ GoodReads/app/src/main/java/com/example/philippe/goodreads/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.philippe.goodreads;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.Window;
 
 import java.io.IOException;
@@ -19,16 +23,35 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar appBar = (Toolbar) findViewById(R.id.appBar);
         setSupportActionBar(appBar);
 
-        try {
-            new DownloadFilesTask().execute(new URL("http://www.google.com"));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_download_shelves:
+                new DownloadFilesTask(getApplicationContext()).execute();
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
     }
 }
